7f62f43baa737ebf242b94e20958b1dbf987abea
charles <17625904731@163.com>
2026-03-16 16:19:12 +0800

    chore(reading): 删除投资至简目录下的调试脚本

--- content/reading/touzhi-jianjian/yuanwen/rebuild.py
+++
@@ -1,8 +0,0 @@
-path='D:/Dev_Center/01_Workspace/Personal/vercel-blog/content/reading/touzhi-jianjian/yuanwen/ch4.md'
-with open(path,'r',encoding='utf-8') as f:
-    content = f.read()
-sec4_start = content.find('## \u7b2c\u56db\u8282')
-sec5_start = content.find('---\n\n## \u7b2c\u4e94\u8282')
-before = content[:sec4_start]
-after = content[sec5_start:]
-print(len(before), len(after))
